bc4e51370808b37634e938cd96f83ab01735fd35
SDSC <SDSC@SDSC-PC>
2016-11-30 17:04:52 -0800

    d

--- KPC/src/sw/Demo002.java
+++ KPC/src/sw/Demo002.java
@@ -10,6 +10,7 @@ public class Demo002 {
 		System.out.println("hello Demo002");
 		System.out.println("hello Demo002");
 		
+		System.out.println("hello Demo002");
 		
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
