f0e79936b60d9da4437742f75e6cfb8efd48d4ae
SDSC <SDSC@SDSC-PC>
2016-11-30 17:03:37 -0800

    d

--- KPC/src/sw/Demo002.java
+++ KPC/src/sw/Demo002.java
@@ -1,5 +1,7 @@
 package sw;
 
+import java.util.Iterator;
+
 public class Demo002 {
 
 	public static void main(String[] args) {
@@ -7,6 +9,11 @@ public class Demo002 {
 		System.out.println("hello Demo002");
 		System.out.println("hello Demo002");
 		System.out.println("hello Demo002");
+		
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
