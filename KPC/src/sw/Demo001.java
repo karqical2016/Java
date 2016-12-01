802bb20b1ddeee9e9f0755dfbf460c9ed2246472
SDSC <SDSC@SDSC-PC>
2016-11-30 16:18:48 -0800

    test

---
+++ KPC/src/sw/Demo001.java
@@ -0,0 +1,10 @@
+package sw;
+
+public class Demo001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Demo001");
+	}
+
+}
---
+++ KPC/src/sw/Demo002.java
@@ -0,0 +1,12 @@
+package sw;
+
+public class Demo002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Demo002");
+		System.out.println("hello Demo002");
+		System.out.println("hello Demo002");
+	}
+
+}
