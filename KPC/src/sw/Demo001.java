693e87e7fe568bf419effcb75c3bfba98d618d8e
SDSC <SDSC@SDSC-PC>
2016-11-30 16:23:02 -0800

    w

--- KPC/src/sw/Demo001.java
+++ KPC/src/sw/Demo001.java
@@ -5,6 +5,8 @@ public class Demo001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello Demo001");
+		System.out.println("hello Demo001");
+		System.out.println("hello Demo001");
 	}
 
 }
