02855166265dc9bc04f671f35b3adafbac0bf01d
SDSC <SDSC@SDSC-PC>
2016-12-01 11:09:38 -0800

    ss

---
+++ KPC/src/com/imooc/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.imooc;
+import java.util.Scanner;
+public class HelloWorld {
+	public static void main(String [] args){
+		
+		Scanner input=new Scanner(System.in);
+		
+		int sum=input.nextInt();
+		
+	    System.out.println(sum);
+	    System.out.println(sum);
+	    System.out.println(sum);
+	    System.out.println(sum);
+	    System.out.println(sum);
+	    System.out.println(sum);
+    	
+	}
+}
