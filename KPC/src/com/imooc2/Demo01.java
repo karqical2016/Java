c21079a6bd807e1596424d4f0b713356cf59b152
Karl <karqical@qq.com>
2016-12-02 11:17:59 -0800

    Java 中的 static 使用之静态变量

---
+++ KPC/src/com/imooc2/Demo01.java
@@ -0,0 +1,13 @@
+package com.imooc2;
+
+public class Demo01 {
+
+	// 定义静态变量，保存班级名称
+	static String className = "JAVA开发一班";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 访问静态变量，输出班级名称
+		System.out.println( Demo01.className);
+	}
+
+}
