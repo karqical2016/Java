f266425ed169d675acf7881c079d7d11951282d2
Karl <SDSC@SDSC-PC>
2016-12-05 10:08:30 -0800

    定义方法内部类

---
+++ KPC/src/com/imooc2/Demo07.java
@@ -0,0 +1,38 @@
+package com.imooc2;
+
+public class Demo07 {
+
+	 private String name = "爱慕课";
+	    
+	    // 外部类中的show方法
+	    public void show() { 
+			// 定义方法内部类
+			class MInner {
+				int score = 83;
+				public int getScore() {
+					return score + 10;
+				}
+			}
+	        
+			// 创建方法内部类的对象
+			MInner in=new MInner();
+			
+	        
+	        // 调用内部类的方法
+			int newScore=in.getScore();
+			
+	        
+			System.out.println("姓名：" + name + "\n加分后的成绩：" + newScore);
+		}
+	    
+		// 测试方法内部类
+		public static void main(String[] args) {
+	        
+			// 创建外部类的对象
+	        Demo07 mo=new Demo07();
+	        
+	        // 调用外部类的方法
+			mo.show();
+		}
+
+}
